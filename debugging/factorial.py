a9195840f928d611dd65da03fd8522a04c005669
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-05 11:53:17 -0400

    fix

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import sys
 
+#!/usr/bin/python3
+import sys
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  # FIX: Decrement n to eventually exit loop
     return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
