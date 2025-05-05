184f63823ce68cb55cc7c57c0f5a645227d9d80e
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-05 11:55:24 -0400

    fix

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -14,18 +14,3 @@ def factorial(n):
 f = factorial(int(sys.argv[1]))
 print(f)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./script.py <number>")
-        sys.exit(1)
-    try:
-        num = int(sys.argv[1])
-        if num < 0:
-            print("Factorial is not defined for negative numbers.")
-            sys.exit(1)
-        f = factorial(num)
-        print(f)
-    except ValueError:
-        print("Please provide a valid integer.")
-        sys.exit(1)
